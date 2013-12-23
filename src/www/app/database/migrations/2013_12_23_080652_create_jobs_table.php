181c548d9a770f563cea4ded5f362134482cce67
jasperboyd <jasperboyd@lavabit.com>
2013-12-23 03:24:13 -0500

    Setup files for resume

---
+++ src/www/app/database/migrations/2013_12_23_080652_create_jobs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateJobsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('jobs', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('title'); 
+			$table->string('company'); 
+			$table->string('date_range'); 
+			$table->mediumtext('summary'); 
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('jobs');
+	}
+
+}
---
+++ src/www/app/models/Skill.php
@@ -0,0 +1,7 @@
+<?php
+
+class Skill extends Eloquent {
+	protected $guarded = array();
+
+	public static $rules = array();
+}
